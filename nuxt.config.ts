e4695275e0f65337cf8320d3171c028732ef0b8f
Phann Phearoen <phannphearin2@gmail.com>
2024-06-25 15:36:09 +0700

    add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["vuetify-nuxt-module"]
+  modules: ["vuetify-nuxt-module", "@nuxtjs/sitemap"],
+  site: {
+    url: import.meta.env.VITE_SITE_URL,
+  }
 })
