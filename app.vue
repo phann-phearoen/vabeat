2f14d3fba0d335e14c0169461fb8bf7daaef2e60
Phann Phearoen <phannphearin2@gmail.com>
2024-11-13 09:30:47 +0700

    define head inside nuxt.config.ts

--- app.vue
+++ app.vue
@@ -5,52 +5,5 @@
 </template>
 
 <script setup lang="ts">
-  useHead({
-    title: "VAbeat｜バビート株式会社",
-    htmlAttrs: { lang: "ja" },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content:
-          "バビート株式会社のコーポレートサイトです。良い！と判断したプロダクトをどんどんカタチにすることを掲げています。",
-      },
-      { name: "format-detection", content: "telephone=no" },
-      {
-        hid: "keywords",
-        name: "keywords",
-        content: "バビート株式会社 , VAbeat , バビート",
-      },
-      {
-        hid: "og:title",
-        property: "og:title",
-        content: "VAbeat｜バビート株式会社",
-      },
-      {
-        hid: "og:description",
-        property: "og:description",
-        content:
-          "バビート株式会社のコーポレートサイトです。良い！と判断したプロダクトをどんどんカタチにすることを掲げています。",
-      },
-      {
-        hid: "og:url",
-        property: "og:url",
-        content: "https://vabeat.co.jp",
-      },
-      {
-        hid: "og:type",
-        property: "og:type",
-        content: "website",
-      },
-      {
-        hid: "og:image",
-        property: "og:image",
-        content: `/linkcard.jpg`,
-      },
-      
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
-  })
+  
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,64 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["vuetify-nuxt-module", "@nuxtjs/sitemap"],
+  app: {
+    head: {
+      title: "VAbeat｜バビート株式会社",
+      htmlAttrs: { lang: "ja" },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "バビート株式会社のコーポレートサイトです。良い！と判断したプロダクトをどんどんカタチにすることを掲げています。",
+        },
+        { name: "format-detection", content: "telephone=no" },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content: "バビート株式会社 , VAbeat , バビート",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "VAbeat｜バビート株式会社",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "バビート株式会社のコーポレートサイトです。良い！と判断したプロダクトをどんどんカタチにすることを掲げています。",
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://vabeat.co.jp",
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "website",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: `/linkcard.jpg`,
+        },
+        
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+    }
+  },
+  modules: ["@nuxtjs/sitemap"],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
   site: {
     url: import.meta.env.VITE_SITE_URL,
   }
