a0eab01ed88fdd12475100e5a409a9634c2e5b91
pang-vichhai <pangvichhai@gmail.com>
2025-02-07 10:11:09 +0700

    update new text for catch phrase

--- components/catch-phrases.vue
+++ components/catch-phrases.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="catch-phrases">
-    <svg
+    <!-- <svg
       xmlns="http://www.w3.org/2000/svg"
       width="105%"
       viewBox="0 0 1400 1080"
@@ -11,16 +11,25 @@
         transform="translate(0 -7.625)"
         fill="#ff9710"
       />
+    </svg> -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="105%" viewBox="0 0 1366 750">
+      <path
+        id="cp-background"
+        d="M608.1,647.183c-109.983,0-83.367,91.49-215.417,93.68-66.783,1.108-111.953-21.357-151.437-43.821C202.656,675.089,169.5,653.134,126.9,653.183,40.707,653.283,0,716.508,0,716.508V28.6S42.7,56.628,126.9,55.251,219.149,44.291,337.23,7.757,547.77,75.592,657.867,75.592,800.31,13.951,919.551,7.757,1113.584,58.725,1191.3,61.34,1364.89,28.6,1364.89,28.6V91.1h-.011c-.1,39.711-.747,336.461,1.117,535.3.584,62.327-64.836,88.373-139.493,90.116-36.16.844-66.053-17.01-95.109-34.864-30.935-19.008-60.921-38.017-96.51-34.46C937.4,656.926,929.11,750,820.869,750S718.08,647.183,608.1,647.183Z"
+      />
     </svg>
 
-    <img src="/text01.svg" alt="text01" id="text01" />
+    <img src="/new-text1.svg" alt="text01" id="text01" />
+    <img src="/new-text2.svg" alt="text03" id="text02" />
+
+    <!-- <img src="/text01.svg" alt="text01" id="text01" />
     <img src="/text03.svg" alt="text03" id="text03" />
     <img src="/text04.svg" alt="text04" id="text04" />
     <img src="/text06.svg" alt="text06" id="text06" />
 
     <img src="/text02.svg" alt="text02" id="text02" />
     <img src="/text05.svg" alt="text05" id="text05" />
-    <img src="/text07.svg" alt="text07" id="text07" />
+    <img src="/text07.svg" alt="text07" id="text07" /> -->
   </div>
 </template>
 
@@ -40,41 +49,41 @@ export default defineComponent({
       onEnter: () => {
         background1?.classList.add("animate-background");
         gsap.to("#text01", {
-          left: "5%",
-          transform: "rotate(20deg)",
-          duration: 1.2,
-        });
-        gsap.to("#text03", {
           left: "15%",
-          transform: "rotate(10deg)",
-          top: "47%",
-          duration: 1.2,
-        });
-        gsap.to("#text04", {
-          left: "31%",
-          transform: "rotate(-20deg)",
-          duration: 1.2,
-        });
-        gsap.to("#text06", {
-          left: "2%",
-          transform: "rotate(-15deg)",
+          // transform: "rotate(5deg)",
           duration: 1.2,
         });
         gsap.to("#text02", {
-          right: "5%",
-          transform: "rotate(-15deg)",
+          left: "15%",
+          transform: "rotate(-2deg)",
+          top: "55%",
           duration: 1.2,
         });
-        gsap.to("#text05", {
-          right: "1%",
-          transform: "rotate(12deg)",
-          duration: 1.2,
-        });
-        gsap.to("#text07", {
-          right: "4%",
-          transform: "rotate(-23deg)",
-          duration: 1.2,
-        });
+        // gsap.to("#text04", {
+        //   left: "31%",
+        //   transform: "rotate(-20deg)",
+        //   duration: 1.2,
+        // });
+        // gsap.to("#text06", {
+        //   left: "2%",
+        //   transform: "rotate(-15deg)",
+        //   duration: 1.2,
+        // });
+        // gsap.to("#text02", {
+        //   right: "5%",
+        //   transform: "rotate(-15deg)",
+        //   duration: 1.2,
+        // });
+        // gsap.to("#text05", {
+        //   right: "1%",
+        //   transform: "rotate(12deg)",
+        //   duration: 1.2,
+        // });
+        // gsap.to("#text07", {
+        //   right: "4%",
+        //   transform: "rotate(-23deg)",
+        //   duration: 1.2,
+        // });
       },
     });
   },
@@ -109,11 +118,17 @@ export default defineComponent({
 
 #text01 {
   position: absolute;
-  width: 51vw;
-  left: -34%;
-  top: 10%;
+  width: 65vw;
+  left: -65%;
+  top: 20%;
 }
-#text03 {
+#text02 {
+  position: absolute;
+  width: 60vw;
+  right: -63%;
+  top: 21%;
+}
+/* #text03 {
   position: absolute;
   width: 15vw;
   left: -12%;
@@ -131,13 +146,6 @@ export default defineComponent({
   left: -32%;
   top: 57%;
 }
-
-#text02 {
-  position: absolute;
-  width: 85vw;
-  right: -63%;
-  top: 21%;
-}
 #text05 {
   position: absolute;
   width: 41vw;
@@ -149,7 +157,7 @@ export default defineComponent({
   width: 75vw;
   top: 59%;
   right: -46.5%;
-}
+} */
 
 @media only screen and (max-width: 600px) {
   #catch-phrases {
